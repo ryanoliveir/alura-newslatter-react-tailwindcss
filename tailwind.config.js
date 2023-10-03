1a1754066c1bae24e9cab8027845c06b59c7e7d8
ryanoliveir <ryandehallo229@gmail.com>
2023-10-02 22:55:06 -0300

    :construction: Animation
    add: css animation on sign icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,30 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-
       colors: {
         blue: {
           light: "#C5dFFF",
           dark: "#061E3C",
           hover: "#1057B0",
         },
-        white: '#ffffff'
+        white: "#ffffff",
       },
-    },
-    fontFamily: {
-      inter: ["Inter", "sans-serif"],
+      keyframes: {
+        sino_kf: {
+          "0%, 100%": {
+            transform: "rotate(-10deg)",
+          },
+          "50%": {
+            transform: "rotate(10deg)",
+          },
+        },
+      },
+      animation: {
+        sino: "sino_kf 0.31s ease-in-out infinite",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
     },
   },
   // plugins: [require("@tailwindcss/forms")],
